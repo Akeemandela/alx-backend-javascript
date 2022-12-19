dfeca5e8785f0f5e0216545c5b8f72f8ef479776
Akeemandela <easytwenty14@gmail.com>
2022-12-19 21:26:20 +0000

    This is my first backend javascript drill

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,10 +1,5 @@
 export default function createReportObject(employeesList) {
-  return {
-    allEmployees: { ...employeesList },
-    getNumberOfDepartments() {
-      return Object.keys(employeesList).length;
-    },
-  };
+  return { allEmployees: { ...employeesList } };
 }
 ~
 ~
