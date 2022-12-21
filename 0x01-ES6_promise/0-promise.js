a1475e772b5907f45cbe00e8471e8de23efeb364
Akeemandela <easytwenty14@gmail.com>
2022-12-21 19:33:11 +0000

    Updated ES6 task

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+export default function getResponseFromAPI() {
+  const promiseMade = new Promise(
+    (resolve) => {
+      resolve();
+    },
+  );
+  return promiseMade;
+}
---
+++ 0x01-ES6_promise/README.md
@@ -0,0 +1,2 @@
+This is task is aimed at sharpening my skill in the concept of 
+promise and async await.
