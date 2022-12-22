cbfb198d619dd094af29b2c875521c56361964ee
Akeemandela <easytwenty14@gmail.com>
2022-12-22 16:48:15 +0000

    Javascript classes drill

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,4 +1,11 @@
 import ClassRoom from './0-classroom';
+
 export default function initializeRoom () {
-  return this._maxStudentsSize = [...this._maxStudentsSize, ClassRoom];
+  const maxSizes = [19, 20, 34];
+  const arrOfRoom = [];
+
+  for (const size of maxSizes) {
+    arrOfRoom.push(new ClassRoom(size));
+  }
+  return arrOfRoom;	
 }
