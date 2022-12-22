12bf47d11d5cc64b86eb3b7edb2cd985ba172a02
Akeemandela <easytwenty14@gmail.com>
2022-12-22 19:57:16 +0000

    Javascript classes drill

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -9,7 +9,7 @@ export default class EVCar extends Car {
     this._range = range;
   }
 
- 
+  // Methods
 
   cloneCar() {
     const NewObj = this.constructor[Symbol.species] || this.constructor;
@@ -17,5 +17,13 @@ export default class EVCar extends Car {
     return clone;
   }
 
-  
+  // Setters
+
+  // Getters
 }
+/* class TestCar extends Car {};
+const tc1 = new TestCar("Nissan", "Turbo", "Pink");
+const tc2 = tc1.cloneCar();
+console.log(tc1 instanceof TestCar);
+console.log(tc2 instanceof TestCar);
+console.log(tc1 == tc2); */
